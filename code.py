8e245aec5ee21a45ff53987912612e28ce5161af
WTT <115847026+Marektek@users.noreply.github.com>
2024-05-06 23:19:45 +0200

    Add files via upload
    The code

---
+++ code.py
@@ -0,0 +1,94 @@
+import rgbkeypad
+import socketpool
+import ssl
+import adafruit_requests 
+import wifi
+import time
+from random import randint
+
+#ssid = 'Labor 2.0'
+#password = 'nerdhoehle2'
+ssid = 'MagentaWLAN-VXPV'
+password = '57400371280026405925'
+wifi.radio.connect(ssid,password)
+
+pool = socketpool.SocketPool(wifi.radio)
+requests = adafruit_requests.Session(pool)
+
+keypad = rgbkeypad.RGBKeypad()
+
+#ip="172.16.0.101"
+ip="192.168.2.155"                                              
+
+
+while True:
+    for key in keypad.keys:
+        if key.is_pressed():
+            print("key", key.x, key.y, "pressed")
+            
+            if key.x==0 and key.y==0:
+                print ("pressed")
+                url=f"http://{ip}/win&A=127&FX=15"
+                print (url)
+                key.color = (255, 0, 0)
+                requests.get(url)
+                key.color = (0, 255, 0)
+                time.sleep(1)
+                key.color = (0, 0, 0) 
+            if key.x==2 and key.y==3:
+                print ("pressed")
+                url=f"http://{ip}/win&&T=2"
+                print (url)
+                key.color = (255, 0, 0)
+                requests.get(url)
+                key.color = (0, 255, 0)
+                time.sleep(1)
+                key.color = (0, 0, 0)
+            if key.x==1 and key.y==3:
+                print ("pressed")
+                url=f"http://{ip}/win&&RB"
+                print (url)
+                key.color = (255, 0, 0)
+                requests.get(url)
+                key.color = (0, 255, 0)
+                time.sleep(1)
+                key.color = (0, 0, 0)
+            if key.x==1 and key.y==0:
+                print ("pressed")
+                url=f"http://{ip}/win&&FX=10"
+                print (url)
+                key.color = (255, 0, 0)
+                requests.get(url)
+                key.color = (0, 255, 0)
+                time.sleep(1)
+                key.color = (0, 0, 0)
+            if key.x==2 and key.y==0:
+                print ("pressed")
+                url=f"http://{ip}/win&&FX=159"
+                print (url)
+                key.color = (255, 0, 0)
+                requests.get(url)
+                key.color = (0, 255, 0)
+                time.sleep(1)
+                key.color = (0, 0, 0)
+            if key.x==3 and key.y==0:
+                print ("pressed")
+                url=f"http://{ip}/win&&FX=0"
+                print (url)
+                key.color = (255, 0, 0)
+                requests.get(url)
+                key.color = (0, 255, 0)
+                time.sleep(1)
+                key.color = (0, 0, 0)
+            if key.x==0 and key.y==1:
+                print ("pressed")
+                url=f"http://{ip}/win&&FX=9"
+                print (url)
+                key.color = (255, 0, 0)
+                requests.get(url)
+                key.color = (0, 255, 0)
+                time.sleep(1)
+                key.color = (0, 0, 0)
+
+
+            
